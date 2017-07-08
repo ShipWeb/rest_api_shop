ca9a8dd533aebabefa5c2f99c261d266ca3a4253
NataliShip <Natulechka_m.25@mail.ru>
2017-07-08 19:55:25 +0300

    Add changes to Catalog. Переключение отображения товаров со списка на блоки.

--- views/js/main.js
+++ views/js/main.js
@@ -37,8 +37,33 @@ $("document").ready(function () {
 		var screen_size = $(document).width();
 		if (screen_size < 767) {
 			$('.filter>div').removeClass('in');
-			$('.fa.fa-angle-up').removeClass('rotate');
+			$('.fa.fa-angle-up').removeClass("rotate");
 		}
+	});
+
+	$('.sort_item').click(function () {
+		var sortText = $(this).children("li").html();
+		$('.sort_type').html(sortText);
+		$('#sort_list').removeClass('open');
+		return false;
+	});
+
+	$('#net').click(function () {
+		$('[data-view]').addClass('module_view');
+		$('.column hr').addClass('displayNone');
+		$('.main_search').removeClass('preview');
+		$('.main_search .item_price').removeClass('pull-right');
+		$('#list').removeClass('active_state');
+		$('#net').addClass('active_state');
+	});
+
+	$('#list').click(function () {
+		$('[data-view]').removeClass('module_view');
+		$('.column hr').removeClass('displayNone');
+		$('.main_search').addClass('preview');
+		$('.main_search .item_price').addClass('pull-right');
+		$('#net').removeClass('active_state');
+		$('#list').addClass('active_state');
 	});
 
 });
