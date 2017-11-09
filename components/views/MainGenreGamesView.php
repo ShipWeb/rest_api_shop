6b639209da3f7d7a8a7639477ef2ab7fed9bfb86
NataliShip <Natulechka_m.25@mail.ru>
2017-11-09 18:30:58 +0300

    Исправление отображения главной страницы на мобильных устройствах

--- components/views/MainGenreGamesView.php
+++ components/views/MainGenreGamesView.php
@@ -1,10 +1,10 @@
 <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 aside_menu">
 	<!--Боковое меню-->
-	<a href="<?= Yii::$app->homeUrl ?>product"><h4>Каталог</h4></a>
+	<a href="<?= Yii::$app->homeUrl ?>product"><h4 class="katalog">Каталог</h4></a>
 	<hr class="hidden-xs">
 	<ul class="hidden-xs">
 <?php foreach ($genres as $key => $value): ?>
-	<a href="<?= Yii::$app->homeUrl.'product?'.$value['property_name'].'='.$value['value_str']?>"><li><?=$value['value_str']?></li></a>
+	<a href="<?= Yii::$app->homeUrl.'product?'.$value['property_name'].'='.$value['value_str']?>"><li class="genre"><?=$value['value_str']?></li></a>
 
 <?php endforeach; ?>
 	</ul>
